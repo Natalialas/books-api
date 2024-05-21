b63e1697eace397bfe40433d155e89534ea0e430
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-05-21 12:41:04 +0200

    Add logout endpoint, config module ale users role

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorsModule } from './authors/authors.module';
 import { BooksModule } from './books/books.module';
-import { UsersModule } from './users/users.module';
+import { UserModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [AuthorsModule, BooksModule, UsersModule, AuthModule, PrismaModule],
+  imports: [
+    AuthorsModule,
+    BooksModule,
+    UserModule,
+    AuthModule,
+    PrismaModule,
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,15 @@
 import {
   Controller,
+  Delete,
   Get,
   NotFoundException,
   Param,
   ParseUUIDPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AdminAuthGuard } from 'src/auth/admin-auth.guard';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -21,5 +25,15 @@ export class UsersController {
     const user = await this.usersService.getUsersById(id);
     if (!user) throw new NotFoundException('User not found');
     return user;
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminAuthGuard)
+  @UseGuards(JwtAuthGuard)
+  public async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.usersService.getUsersById(id)))
+      throw new NotFoundException('User not found');
+    await this.usersService.deleteUserById(id);
+    return { success: true };
   }
 }
