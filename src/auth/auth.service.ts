dfd65d47c07310b6e3e20055d7cce461414bb22e
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-05-21 12:41:04 +0200

    Add logout endpoint, config module ale users role

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,12 +3,14 @@ import { UsersService } from 'src/users/users.service';
 import { RegisterDTO } from './dtos/register.dto';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
+    private userService: UsersService,
     private jwtService: JwtService,
+    private configService: ConfigService,
   ) {}
 
   public async register(registrationData: RegisterDTO) {
@@ -16,11 +18,12 @@ export class AuthService {
     const userData = {
       email: registrationData.email,
     };
-    return this.usersService.addNewUser(userData, hashedPassword);
+
+    this.userService.addNewUser(userData, hashedPassword);
   }
 
   public async validateUser(email: string, password: string) {
-    const user = await this.usersService.getByEmail(email);
+    const user = await this.userService.getByEmail(email);
     if (
       user &&
       (await bcrypt.compare(password, user.password.hashedPassword))
@@ -30,13 +33,12 @@ export class AuthService {
     }
     return null;
   }
-
   public async createSession(user: any) {
     const payload = { email: user.email, sub: user.id };
 
     const accessToken = this.jwtService.sign(payload, {
-      secret: 'xrwe4543534',
-      expiresIn: '12h',
+      secret: this.configService.get('jwt.secret'),
+      expiresIn: this.configService.get('jwt.expiresIn'),
     });
 
     return {
