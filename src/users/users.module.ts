43d98ae97cd55cfab8cfa0df514e87919b55ef0e
Natalia Laszczkowska <natalia.laszczkowskax@gmail.com>
2024-05-21 11:02:23 +0200

    Add auth module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { PrismaService } from 'src/shared/services/prisma.service';
+import { UsersController } from './users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  providers: [UsersService],
+  imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, PrismaService],
+  exports: [UsersService],
 })
-export class UsersModule {}
+export class UserModule {}
